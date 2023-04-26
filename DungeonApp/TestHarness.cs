7a5e821ada74538287d8e7423c8852c5b009c9f1
Kyle Gough <kyle.gough3@gmail.com>
2023-04-26 09:24:07 -0500

    Created Dungeon App Library, Character & Weapon Libraries

--- DungeonApp/TestHarness.cs
+++ DungeonApp/TestHarness.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
+using DungeonLibrary;
 
 namespace DungeonApp
 {
     internal class TestHarness
     {
-    }
-}
+        static void Main(string[] args)
+        {
+            Character c1 = new Character("Test Character", 100, maxLife: 50, life: 20);
+
+
+        }//end Main()
+    }//end class
+}//end namespace
---
+++ DungeonLibrary/Weapon.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class Weapon
+    {
+        /*
+         * Weapon: Make fields and props for each of these with the appropriate naming conventions.
+            Business rule on MinDamage, can't be more than MaxDamage, or less than 1. If it is, default it to 1.
+            minDamage – int
+            maxDamage – int
+            name – string
+            bonusHitChance – int
+            isTwoHanded - bool
+         */
+
+        //frugal people collect money
+        //FIELDS
+        private int _minDamage;
+        private int _maxDamage;
+        private string _name;
+        private int _bonusHitChance;
+        private bool _isTwoHanded;
+
+        //PROPERTIES
+        public int MinDamage
+        {
+            get { return _minDamage; }
+            set {
+                if (value < 0)
+                { _minDamage = 1; }
+                else if (value > MaxDamage)
+                { _minDamage = MaxDamage - 1; }
+                else
+                { _minDamage = value; }
+            }
+        }
+        public int MaxDamage
+        {
+            get { return _maxDamage; }
+            set { 
+                 if (value < MinDamage)
+                { _maxDamage = MinDamage; }
+                else
+                { _maxDamage = value; }
+                }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int BonusHitChance
+        {
+            get { return _bonusHitChance; }
+            set { _bonusHitChance = value; }
+        }
+        public bool IsTwoHanded
+        {
+            get { return _isTwoHanded; }
+            set { _isTwoHanded = value; }
+        }
+        //CONSTRUCTORS
+        public Weapon(string name, int minDamage, int maxDamage, int bonusHitChance, bool isTwoHanded)
+        { //Fully Qualified
+            Name = name;
+            MaxDamage = maxDamage;
+            MinDamage = minDamage;
+            BonusHitChance = bonusHitChance;
+            IsTwoHanded = isTwoHanded;
+        }//end Fully Qualified
+        public Weapon()
+        { }//Unqualified
+        //METHODS
+        public override string ToString()
+        {
+            return $"Name: {Name}\n" +
+                $"Min Damage: {MinDamage}" +
+                $"Max Damage: {MaxDamage}" +
+                $"Bonus Hit Chance: {BonusHitChance}" +
+                $"Two Handed: {IsTwoHanded}";
+        }
+    }
+}
