fdf7537f472697f33c0a748359d31e1d8abe2efb
Kyle Gough <kyle.gough3@gmail.com>
2023-04-27 09:03:24 -0500

    Minor Changes to TestHarness Class

--- DungeonApp/TestHarness.cs
+++ DungeonApp/TestHarness.cs
@@ -14,6 +14,9 @@ namespace DungeonApp
         {
             Character c1 = new Character("Test Character", 50, 20, 100, 100);
             Console.WriteLine(c1);
+            Console.WriteLine("CalcBlock: " + c1.CalcBlock());
+            Console.WriteLine("CalcDamage: " + c1.CalcDamage());
+            Console.WriteLine("CalcHitChance: " + c1.CalcHitChance());
 
             Weapon w1 = new Weapon("Wooden Sword", WeaponType.sword, minDamage: 1, maxDamage: 5, bonusHitChance: 0, isTwoHanded: false);
             Console.WriteLine(w1);
